06bb8f6427147d9c8d74e51c39de9abd1de0bd87
nicolas de paula <129803875+nicolasdepaulla@users.noreply.github.com>
2024-05-06 17:22:20 -0300

    Add files via upload

---
+++ exercicios/media/logica.js
@@ -0,0 +1,17 @@
+/**
+ * JS - Simulador de média de notas
+ * @author nicolas de paula
+ */
+
+let media = 4
+
+
+
+function testar() {
+    //frmmedia.txtteste.value= "Aprovado"
+    if (media >= 7){
+        alert("Aprovado")
+    }else{
+        alert("Reprovado")
+    }
+}

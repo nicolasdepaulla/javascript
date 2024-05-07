748ff4b86496e53a96127d2616247a8b54323b35
nicolas de paula <129803875+nicolasdepaulla@users.noreply.github.com>
2024-05-07 17:31:50 -0300

    Add files via upload

--- exercicios/media/logica.js
+++ exercicios/media/logica.js
@@ -1,17 +1,26 @@
-/**
- * JS - Simulador de média de notas
- * @author nicolas de paula
- */
+/*
+@author nicolas de paula pacheco
+*/
 
-let media = 4
+let num1, num2, num3, num4 
+function calcular() {
+    num1 = Number(frmflex.textn1.value)   
+    num2 = Number(frmflex.textn2.value) 
+    num3 = Number(frmflex.textn3.value)
+    num4 = Number(frmflex.textn4.value)
 
-
-
-function testar() {
-    //frmmedia.txtteste.value= "Aprovado"
-    if (media >= 7){
-        alert("Aprovado")
-    }else{
-        alert("Reprovado")
+    //validação de campos obrigatorio
+    if (frmflex.textn1.value === "") {
+        alert("prencha o valor da n1")
+        frmflex.textn1.value.focus()
+    } else if (frmflex.textn2.value === "") {
+        alert("prencha o valor da n2")
+        frmflex.textn2.value.focus()
+    } else if (frmflex.textn3.value === "") {
+        alert("prencha o valor da n3")
+        frmflex.textn3.value.focus()
+    } else if (frmflex.textn4.value === "") {
+        alert("prencha o valor da n4")
+        frmflex.textn4.value.focus() }
+    
     }
-}

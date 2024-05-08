d9ca35308f566f98aa4c8cffbb2ea88d2985a29d
nicolas de paula <129803875+nicolasdepaulla@users.noreply.github.com>
2024-05-08 13:58:05 -0300

    Add files via upload

--- exercicios/media/logica.js
+++ exercicios/media/logica.js
@@ -2,25 +2,38 @@
 @author nicolas de paula pacheco
 */
 
-let num1, num2, num3, num4 
+let num1, num2, num3, num4
 function calcular() {
-    num1 = Number(frmflex.textn1.value)   
-    num2 = Number(frmflex.textn2.value) 
+    num1 = Number(frmflex.textn1.value)
+    num2 = Number(frmflex.textn2.value)
     num3 = Number(frmflex.textn3.value)
     num4 = Number(frmflex.textn4.value)
 
+    const resultado =(num1 + num2 + num3 + num4) / 4
+
     //validação de campos obrigatorio
     if (frmflex.textn1.value === "") {
-        alert("prencha o valor da n1")
+        alert("prencha o valor de num1")
         frmflex.textn1.value.focus()
     } else if (frmflex.textn2.value === "") {
-        alert("prencha o valor da n2")
+        alert("prencha o valor de num2")
         frmflex.textn2.value.focus()
     } else if (frmflex.textn3.value === "") {
-        alert("prencha o valor da n3")
+        alert("prencha o valor de num3")
         frmflex.textn3.value.focus()
     } else if (frmflex.textn4.value === "") {
-        alert("prencha o valor da n4")
-        frmflex.textn4.value.focus() }
-    
+        alert("prencha o valor de num4")
+        frmflex.textn4.value.focus()
+    } else {
+        if(resultado < 4 ) {
+            document.getElementById('status').src = "./img/reprovado.png"
+            alert(`A média do aluno é ${resultado}, ele está reprovado!`)
+        }else if (resultado >= 7){
+            document.getElementById('status').src = "./img/aprovado.png"
+            alert(`A média do aluno é ${resultado}, ele está aprovado!`)
+        }else {
+            document.getElementById('status').src = "./img/recuperacao.png"
+            alert(`A média do aluno é ${resultado}, ele está de recuperação!`)
     }
+}
+}

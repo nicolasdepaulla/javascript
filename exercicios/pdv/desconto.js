ebc94a37ec38b26246ee3d63f2f15e3b7feba1c2
nicolas de paula <129803875+nicolasdepaulla@users.noreply.github.com>
2024-05-15 17:16:42 -0300

    Add files via upload

--- exercicios/pdv/desconto.js
+++ exercicios/pdv/desconto.js
@@ -10,8 +10,21 @@ function calcular() {
     pago = Number(frmflex.txtvalorPago.value)
     troco = Number(frmflex.txtTroco.value)
 
-    const desconto =(total % desconto)
+    const total = (total)
+    const desconto = (desconto)
 
+    //validação de campos obrigatorio
+    if (frmflex.txtTotal.value === "") {
+        frmflex.txtTotal.value.focus()
+    } else if (frmflex.txtPorcentagem.value === "") {
+        frmflex.txtPorcentagem.value.focus()
+    } else if (frmflex.txtvalorPago.value === "") {
+        frmflex.txtvalorPago.value.focus()
+    } else if (frmflex.txtTroco.value === "") {
+        frmflex.txtTroco.value.focus()
+    }
     
-    
+}
+function limpar() {
+    document.getElementById('status').src = "imgg/flex.png"
 }
